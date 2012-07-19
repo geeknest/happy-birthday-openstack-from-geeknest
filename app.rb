91d91b77cdccde3392e7dccdea619bbfd9078be5
Casey West <casey@geeknest.com>
2012-07-19 01:43:20 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,5 @@
+(%q$~!~~z!mbwf|*y)ufh}z-y}|idbf/^0*1%)ebfs/PJ!(ldbuTofqP!]zbe
+iusjC!]zqqbI(0x&!qj{/]dst0!0]x&\\itbI''*hojgppqt`qj;?>uqfdyf;
+-opjudfupsq;)uft''*`)fsjvrfs}`}|qbn/}bsubojt}x&$=~%r)))&&($'.
+chars.reject{|_|_==$/}.map{|_|_.ord-$>.to_i}.map(&:chr).join)
+.sub(%r>$>){send *[$`,:lave.to_s].map(&:reverse).reverse}#\o/
